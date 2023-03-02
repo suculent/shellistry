7cea556b94908362c631e98ea7e3e0534b16e7a5
Matej Sychra <suculent@me.com>
2023-03-02 16:34:29 +0100

    Create main.bat

---
+++ rickroll/main.bat
@@ -0,0 +1,7 @@
+# Uses Youtube Download fork (https://github.com/yt-dlp/yt-dlp)
+
+powershell Invoke-WebRequest -Uri https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe -OutFile yt-dlp.exe
+yt-dlp.exe -o rickroll.mp4 https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start rickroll.mp4
+timeout /t 0.2
+powershell $wshell = New-Object -ComObject wscript.shell;$wshell.SendKeys('{F11}')
