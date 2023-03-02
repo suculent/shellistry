6cb835b00a414d8b6480fa345bc7c946862da76d
Matej Sychra <suculent@me.com>
2023-03-02 14:14:15 +0100

    Update echo.ps1

--- echo.ps1
+++ echo.ps1
@@ -1,4 +1,4 @@
 # Usage: powershell -nop -c "iex(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/suculent/shellistry/master/echo.ps1')"
 
-Write-Host -ForegroundColor DarkGreen -BackgroundColor White -NoNewline '[ OK ]'
-Write-Host -ForegroundColor White -BackgroundColor DarkGreen 'We are watching you.'
+Write-Host -ForegroundColor White -BackgroundColor DarkGreen -NoNewline '[ OK ]'
+Write-Host -ForegroundColor White -BackgroundColor Blue ' We are watching you.'
